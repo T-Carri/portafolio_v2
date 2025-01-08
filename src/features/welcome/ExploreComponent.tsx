1fa27bcf5295f4214273c40751b2ccab191ead3a
Mau <bryan_narutito@hotmail.com>
2025-01-07 21:50:42 -0600

    070120250-950

--- src/features/welcome/ExploreComponent.tsx
+++ src/features/welcome/ExploreComponent.tsx
@@ -1,12 +1,13 @@
 import { useTheme } from '../../context/uiContext';
 import '../../styles/components/explorecomponent.scss'
-
+import imgtopone from '../../assets/7783504.jpg'
 const ExploreComponent = () => {
     const { theme } = useTheme();
 
   return (
     <div className={`welcome_explore_component  ${theme==='dark'?'dark-theme': ''}`} >
-        ExploreComponent
+    <div className='space_one' style={{ backgroundImage: `url(${imgtopone})` }}>x</div>
+    <div>y</div>
     </div>
   )
 }
