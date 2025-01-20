1ba3dab11b0b3fea10a62ef8cf31b957e902f8db
Mau <bryan_narutito@hotmail.com>
2025-01-20 00:05:14 -0600

    20012025

--- src/features/welcome/ExploreComponent.tsx
+++ src/features/welcome/ExploreComponent.tsx
@@ -1,13 +1,24 @@
 import { useTheme } from '../../context/uiContext';
 import '../../styles/components/explorecomponent.scss'
 import imgtopone from '../../assets/patterTarjeta.svg'
+import peep from '../../assets/peep.png'
 const ExploreComponent = () => {
     const { theme } = useTheme();
 
   return (
     <div className={`welcome_explore_component  ${theme==='dark'?'dark-theme': ''}`} >
     <div className='space_one' style={{ backgroundImage: `url(${imgtopone})` }}></div>
-    <div className='explore_card'>etst</div>
+    <div className='explore_card'>
+<div className='explore_card_img'>
+  <img src={peep}  alt="Bryan Carrillo programador web imagen de identidad divertida" width={300} />
+</div>
+<div className='explore_card_info'>
+  <div></div>
+  <div className='data_card'>
+    
+  </div>
+</div>
+    </div>
     </div>
   )
 }
